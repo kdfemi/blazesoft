bd7e045f41d0ac79458b10285fecfd29b059cd2d
Femi Kolade <koladefemi@gmail.com>
2023-12-27 23:14:25 -0600

    Added Prompt to Delete and fixed propagation bug fix

--- src/components/book/index.tsx
+++ src/components/book/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from "next/image";
-import { FC, useRef } from "react";
+import { FC, MouseEvent, useRef } from "react";
 import { IBook } from "src/types/book";
 import photo from 'src/assets/image/product_photo.png';
 import Button from "../button";
@@ -12,6 +12,14 @@ const Book: FC<IBook> = (book) => {
     const {category, description, id, name, price} = book;
     const modalRef = useRef<IManageBook>(null);
     const dispatch = useAppDispatch();
+
+    const handleDelete = (e: MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+        if(confirm('Are you sure you want to delete Book')) {
+            dispatch(deleteBook({id}))
+        }
+    };
+
     return (
         <div aria-label="book"  data-testid="book" aria-description={description} className="flex gap-x-4 cursor-default w-[280px] max-w-full" onClick={() => modalRef.current?.showModal(book)}>
             <div className="w-[180px] h-[250px]">
@@ -23,7 +31,7 @@ const Book: FC<IBook> = (book) => {
                     <p  aria-label="category" className="text-text text-xs"><b aria-hidden>Category: </b><span className="text-text/50">{category}</span></p>
                     <p aria-label="price" className="text-text text-xs">CAD {price}</p>
                 </div>
-                <Button aria-label="Delete Book" onClick={() => dispatch(deleteBook({id}))}>Delete</Button>
+                <Button aria-label="Delete Book" onClick={handleDelete}>Delete</Button>
             </div>
             <ManageBook ref={modalRef} />
         </div>
